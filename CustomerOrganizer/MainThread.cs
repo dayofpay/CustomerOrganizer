b509bee807caad2f779872ca3afb24f1e517451e
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-04-07 05:36:51 +0300

    OOP Structure optimisation

--- CustomerOrganizer/MainThread.cs
+++ CustomerOrganizer/MainThread.cs
@@ -34,16 +34,66 @@ namespace CustomerOrganizer
                 updateData();
             }
         }
+        public class Customer
+        {
+            public string FirstName { get; set; }
+            public string SecondName { get; set; }
+            public string ThirdName { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Fax { get; set; }
+            public string Email { get; set; }
+            public string Address { get; set; }
+            public int Age { get; set; }
+            public string Sex { get; set; }
+
+            public Customer(string firstName, string secondName, string thirdName, string phoneNumber, string fax, string email, string address, int age, string sex)
+            {
+                FirstName = firstName;
+                SecondName = secondName;
+                ThirdName = thirdName;
+                PhoneNumber = phoneNumber;
+                Fax = fax;
+                Email = email;
+                Address = address;
+                Age = age;
+                Sex = sex;
+            }
+        }
+
         public void updateData()
         {
-            this.customers.Rows.Clear();
-            MySqlCommand getNames = new MySqlCommand("SELECT * FROM customers",Database.Connections.db.connection);
+            List<Customer> customerList = new List<Customer>();
+            MySqlCommand getNames = new MySqlCommand("SELECT * FROM customers", Database.Connections.db.connection);
             using (MySqlDataReader readNames = getNames.ExecuteReader())
             {
                 while (readNames.Read())
                 {
-                    this.customers.Rows.Add(readNames.GetString(0), readNames.GetString(1), readNames.GetString(2), readNames.GetString(3), readNames.GetString(4),readNames.GetString(5), readNames.GetString(6), readNames.GetString(7), readNames.GetString(8));
+                    Customer customer = new Customer(
+                        readNames.GetString(0),
+                        readNames.GetString(1),
+                        readNames.GetString(2),
+                        readNames.GetString(3),
+                        readNames.GetString(4),
+                        readNames.GetString(5),
+                        readNames.GetString(6),
+                        readNames.GetInt32(7),
+                        readNames.GetString(8));
+                    customerList.Add(customer);
                 }
+            }
+
+            foreach (Customer customer in customerList)
+            {
+                this.customers.Rows.Add(
+                    customer.FirstName,
+                    customer.SecondName,
+                    customer.ThirdName,
+                    customer.PhoneNumber,
+                    customer.Fax,
+                    customer.Email,
+                    customer.Address,
+                    customer.Age,
+                    customer.Sex);
             }
         }
 
@@ -51,9 +101,9 @@ namespace CustomerOrganizer
 
         private void getCustomers_Click(object sender, EventArgs e)
         {
-            updateData();
             customers.Visible = true;
         }
+
 
         private void insertCustomers_Click(object sender, EventArgs e)
         {
